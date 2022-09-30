b624f0246c35fdcfbb0f25ca4decc4cb071154b5
Rishab Dugar <www.anitadugar9163@gmail.com>
2022-09-30 23:45:53 +0530

    Added a download button #9

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import DownloadButton from './components/DownloadButton';
+
+function App() {
+  return (
+    <DownloadButton></DownloadButton>
+  );
+}
+
+export default App;
