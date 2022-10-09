2545567f797ff1b1fd98bdd11a5f92b7652cfda0
Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>
2022-10-10 01:53:35 +0600

    Create component is in format of DIN A4 and take small ratio
    Signed-off-by: Md. Musfiqur Rahaman <musfiqur.rahaman@northsouth.edu>

--- src/components/CV.js
+++ src/components/CV.js
@@ -1,1 +1,19 @@
+import React from "react";
 
+const DisplayCV = () => {
+  return (
+    <div
+      style={{
+        width: "calc(2480px/4)",
+        height: "calc(3508px/4)",
+        border: "2px solid black",
+        margin: "50px",
+        padding: "20px",
+      }}
+    >
+      <h1>Your Name</h1>
+    </div>
+  );
+};
+
+export default DisplayCV;
