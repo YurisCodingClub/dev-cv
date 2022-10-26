ac8ea3454b8d95debb69f19cadd35e513eb727f6
Haris Shah <harisshah385@gmail.com>
2022-10-26 14:52:12 +0500

    feature-31[add skiplink]

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import "./App.css";
-import Header from "./components/Header";
-import DownloadButton from "./components/DownloadButton";
-import ProgrammingExpertise from "./components/ProgrammingExpertise";
-import Education from "./components/Education";
-import PersonalInformation from "./components/PersonalInformation";
-import CV from './components/CV';
-import Skills from "./components/Skills";
+import './App.css'
+import Header from './components/Header'
+import DownloadButton from './components/DownloadButton'
+import ProgrammingExpertise from './components/ProgrammingExpertise'
+import Education from './components/Education'
+import PersonalInformation from './components/PersonalInformation'
+import CV from './components/CV'
+import Skills from './components/Skills'
+import SkipLink from './components/SkipLink'
 
 function App() {
   return (
     <>
+      <SkipLink />
       <Header />
-      <DownloadButton />
-      <ProgrammingExpertise />
-      <Education />
-      <PersonalInformation />
-      <Skills/>
-      <CV />
-      
+      <main id='main'>
+        <DownloadButton />
+        <ProgrammingExpertise />
+        <Education />
+        <PersonalInformation />
+        <Skills />
+        <CV />
+      </main>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/components/SkipLink.js
@@ -0,0 +1,10 @@
+import '../SkipLink.css'
+const SkipLink = () => {
+  return (
+    <a href='#main' className='skip-link'>
+      skip to main
+    </a>
+  )
+}
+
+export default SkipLink
