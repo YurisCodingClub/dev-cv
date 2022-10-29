6b9b54c73ddc723480391063dba469a24e914a7e
Haris <harisshah385@gmail.com>
2022-10-29 22:56:05 +0500

    [feature-8] Add localstorage for certificates form

--- src/components/Certificates.js
+++ src/components/Certificates.js
@@ -1,9 +1,29 @@
+import { useState } from 'react'
+
 const Certificates = () => {
+  const [certificate, setCertificate] = useState('')
+  const [certificateInfo, setCertificateInfo] = useState('')
+
+  const hanldeSubmit = (e) => {
+    e.preventDefault()
+    const certificateData = {
+      Certificate: certificate,
+      AdditionalInfo: certificateInfo,
+    }
+    localStorage.setItem('Certificate', JSON.stringify(certificateData))
+  }
+
+  const handleClear = () => {
+    setCertificate('')
+    setCertificateInfo('')
+    localStorage.removeItem('Certificate')
+  }
+
   return (
     <div>
       <h1>Certificate</h1>
 
-      <form>
+      <form onSubmit={hanldeSubmit}>
         <ul>
           <li>
             <label for='form-certificate'>Certificate</label>
@@ -13,6 +33,8 @@ const Certificates = () => {
               id='form-certificate'
               placeholder='Enter certificate Name'
               aria-label='Enter Your certificate Name'
+              value={certificate}
+              onChange={(e) => setCertificate(e.target.value)}
             />
           </li>
           <li>
@@ -23,12 +45,14 @@ const Certificates = () => {
               id='form-additional-info'
               placeholder='Enter Additional Info'
               aria-label='Provide Additional Info about certificate'
+              value={certificateInfo}
+              onChange={(e) => setCertificateInfo(e.target.value)}
             />
           </li>
         </ul>
 
         <button>Save</button>
-        <button>Cancel</button>
+        <button onClick={handleClear}>Cancel</button>
       </form>
     </div>
   )
